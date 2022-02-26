4b80a8c1db3ee3319362b8e5174219e6585a35bd
sravs <sravs@outlook>
2022-02-26 14:23:38 +0000

    agri.java and msme.java added

--- reg.java
+++ reg.java
@@ -1,1 +1,3 @@
-dhow you prepare is more important than how much you prepare 
+dhow you prepare is more important than how much you prepare
+the more push your self out of comfort zone your life is good
+ 
